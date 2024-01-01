321369242e7086a01a218479e99c67abc6c0832e
Will Pierson <willcpierson@gmail.com>
2024-01-01 23:55:15 +0000

    fix gamesRouter

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/api/users');
 const gamesRouter = require("./routes/api/games");
 
 const app = express();
 
+
 app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
-app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/games', gamesRouter);
 
