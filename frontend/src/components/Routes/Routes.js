0c47eecb1233c0ca74d236efcc3026e8e33e0426
Will Pierson <willcpierson@gmail.com>
2024-02-10 23:48:25 +0000

    add frontend routes file

---
+++ frontend/src/components/Routes/Routes.js
@@ -0,0 +1,36 @@
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+// May need editing, user should be able to access whole website except for purchase of games, 
+// and viewing their list of games
+
+export const AuthRoute = ({ component: Component, path, exact }) => {
+  const loggedIn = useSelector(state => !!state.session.user);
+
+  return (
+    <Route path={path} exact={exact} render={(props) => (
+      !loggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/games" />
+      )
+    )} />
+  );
+};
+
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const loggedIn = useSelector(state => !!state.session.user);
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        loggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
