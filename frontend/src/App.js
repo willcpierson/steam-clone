c8a1cc10fc269edcc5e50977a6a37b625fb38030
Will Pierson <willcpierson@gmail.com>
2024-02-13 23:47:59 +0000

    wrap App in Switches, make mainpage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+
+import MainPage from './components/MainPage/MainPage';
+
 function App() {
   return (
-    <h1>Steam Clone</h1>
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
   );
 }
 
---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>A Steam Clone</p>
+        <footer>
+          Copyright &copy; 2024 Steam Clone
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
